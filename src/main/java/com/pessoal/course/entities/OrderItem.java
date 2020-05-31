d9b88588c9093ced9863b5b6853f9721ccc67067
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-05-31 14:33:09 -0300

    Subtotal e total methods

--- src/main/java/com/pessoal/course/entities/OrderItem.java
+++ src/main/java/com/pessoal/course/entities/OrderItem.java
@@ -65,6 +65,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return quantity*price;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
