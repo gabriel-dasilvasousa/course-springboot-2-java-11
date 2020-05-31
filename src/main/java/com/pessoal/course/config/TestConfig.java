dc68355c4bceb95244a3ea1d71d4dedf0b514b06
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-05-31 14:19:22 -0300

    Payment one-to-one association

--- src/main/java/com/pessoal/course/config/TestConfig.java
+++ src/main/java/com/pessoal/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.pessoal.course.entities.Category;
 import com.pessoal.course.entities.Order;
 import com.pessoal.course.entities.OrderItem;
+import com.pessoal.course.entities.Payment;
 import com.pessoal.course.entities.Product;
 import com.pessoal.course.entities.User;
 import com.pessoal.course.entities.enums.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
